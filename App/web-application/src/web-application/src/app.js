659385d8c4789c8bda42e9980e606ef9c50ad139
Daniel Ekeroth <danielekeroth123321@gmail.com>
2020-01-22 21:11:16 +0100

    uploading first initial working part.

---
+++ App/web-application/src/web-application/src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const nodemon = require("nodemon");
+const handlebars = require("handlebars");
+const exphbs = require('express-handlebars');
+const path = require("path");
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+//Initialize handlebars
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  defaultLayout: 'base',
+  layoutsDir: __dirname + '/pl/src/views/layouts/',
+  partialsDir: __dirname + '/pl/src/views/partials/'
+}));
+app.set('views', path.join(__dirname , '/pl/src/views'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render("home", {title: "Home"});
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
