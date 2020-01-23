a719fb0310cb81ad3d3677de6faec441d33be577
Oskar Eek <farssan123@hotmail.se>
2020-01-23 15:10:11 +0100

    Added tabels to database

--- App/web-application/src/web-application/src/app.js
+++ App/web-application/src/web-application/src/app.js
@@ -3,6 +3,7 @@ const nodemon = require("nodemon");
 const handlebars = require("handlebars");
 const exphbs = require('express-handlebars');
 const path = require("path");
+const mysql = require("mysql")
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -11,16 +12,23 @@ const HOST = '0.0.0.0';
 const app = express();
 //Initialize handlebars
 app.engine('hbs', exphbs({
-  extname: 'hbs',
-  defaultLayout: 'base',
-  layoutsDir: __dirname + '/pl/src/views/layouts/',
-  partialsDir: __dirname + '/pl/src/views/partials/'
+    extname: 'hbs',
+    defaultLayout: 'base',
+    layoutsDir: __dirname + '/pl/src/views/layouts/',
+    partialsDir: __dirname + '/pl/src/views/partials/'
 }));
-app.set('views', path.join(__dirname , '/pl/src/views'));
+app.set('views', path.join(__dirname, '/pl/src/views'));
 app.set('view engine', 'hbs');
 
+const db = mysql.createConnection({
+    host: "db",
+    user: "root",
+    password: "abc123",
+    database: "myDB"
+})
+
 app.get('/', (req, res) => {
-  res.render("home", {title: "Home"});
+    res.render("home", { title: "Home" });
 });
 
 app.listen(PORT, HOST);
